1913398aefe560eac3b2e379b36d33743223a95e
ICONIC_07 <souravp9057@gmail.com>
2024-04-26 21:14:36 +0530

    Phising Detection website frontend done

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const PORT = 8000
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Check if the server is running
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
